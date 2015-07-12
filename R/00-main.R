d6701f5eabbbaf6746691b64ad6006583747b297
Sam Boysel <sboysel@gmail.com>
2015-07-12 02:28:10 -0400

    scripts go in R

---
+++ R/00-main.R
@@ -0,0 +1,5 @@
+# Run all files in R/ -------------------------------------
+source("00-deps.R")
+source("01-getdata.R")
+source("02-cleandata.R")
+source("03-analysis.R")
--- main.R
+++
@@ -1,5 +0,0 @@
-# Run all files in src/ -------------------------------------
-source("src/00-deps.R")
-source("src/01-getdata.R")
-source("src/02-cleandata.R")
-source("src/03-analysis.R")
